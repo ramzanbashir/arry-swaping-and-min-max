6b5a70e125959197bce08a3ba16071e1b1e05a33
MUHAMMAD RAMZAN <153890643+ramzanbashir@users.noreply.github.com>
2025-02-17 17:33:25 +0500

    Array-swaping-without-method

---
+++ app.js
@@ -0,0 +1,56 @@
+// let arr = ["karachi","sukhar","multan","hydrabad"];
+// let obj = arr.shift()
+// console.log(obj)
+// console.log(arr);
+
+// let arr = ["Karachi","lahore","Islamabad","Quetta"];
+// arr .shift(0,1);
+// arr .unshift("lahore","karachi");
+// arr.splice(0,2)
+// // arr .shift(1)
+// // arr.unshift("karachi")
+// console.log(arr);
+
+
+// let arr = ["Karachi", "lahore", "Islamabad", "Quetta"];
+
+// chanhge the position 
+// let obj = arr[0];
+// console.log(obj)
+// arr[0] = arr[1]; 
+// arr[1] = obj;  
+
+// console.log(arr);
+
+// var num = [3, 7, 12];
+// var maxValue = Math.max(...num);
+// document.write(`
+//     <h1>
+//     ${maxValue}
+//     </h1>
+// `); 
+
+
+
+// let arr = ["Karachi", "lahore", "Islamabad","Multan","Hydrabad"];
+
+// arr[1] = [arr[3],arr[3] = arr[1]][0]
+// console.log(arr);
+
+
+// var arr = ["karachi", "lahore", "islamabad"];
+// var city = prompt()
+// var milgya = "no"; 
+// for(var i=0; i < arr.length; i++){
+//     if(arr[i] === city) {
+//         milgya = "yes";
+//         document.write(`<h1>city mil gyai hy`);
+//         break
+//     }
+// }
+
+// if(milgya === "no"){
+//     document.write(`<h1>city nhi mili hy</h1>`)
+// }
+
+
